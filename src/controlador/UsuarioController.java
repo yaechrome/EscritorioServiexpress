ca818065c2a3362121cabf1c4dece590baa57c48
Yaeko Ogino <yaekoogino@gmail.com>
2018-07-07 18:58:55 -0400

    Se agrega usuarioController y conexion

---
+++ src/controlador/UsuarioController.java
@@ -0,0 +1,27 @@
+package controlador;
+
+import java.util.List;
+import modelo.Usuario;
+
+public class UsuarioController {
+
+    public boolean Crear(Usuario usuario) {
+        return true;
+    }
+
+    public boolean Modificar(Usuario usuario) {
+        return true;
+    }
+
+    public List<Usuario> ListarTodos() {
+        List<Usuario> usuarios = null;
+        return usuarios;
+    }
+
+    public Usuario buscarPorRut(String rut) {
+        Usuario usuario = null;
+
+        return usuario;
+    }
+
+}
