7be9f21d0ee845e014b77b95d6bb528f83244236
Joselyn Moya <joselyn.moyap@gmail.com>
2018-07-07 19:03:59 -0400

    Se agrega PerfilController

---
+++ src/controlador/PerfilController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.util.List;
+import modelo.Perfil;
+
+/**
+ *
+ * @author nippo
+ */
+public class PerfilController {
+    
+    public List<Perfil> listarPerfiles(){
+        List<Perfil> perfiles = null;
+        
+        return perfiles;
+    }
+    
+    public Perfil buscarPorID(int id){
+        Perfil perfil = null;
+        
+        return perfil;
+    }
+}
