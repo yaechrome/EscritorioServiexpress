a330402dd1ec12d3498d85739a3555a67b99d53b
Joselyn Moya <joselyn.moyap@gmail.com>
2018-07-07 18:33:38 -0400

    se crea clase usuario y perfil

---
+++ src/modelo/Perfil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author nippo
+ */
+public class Perfil {
+    private int id;
+    private String detallePerfil;
+
+    public Perfil() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDetallePerfil() {
+        return detallePerfil;
+    }
+
+    public void setDetallePerfil(String detallePerfil) {
+        this.detallePerfil = detallePerfil;
+    }
+    
+    
+}
---
+++ src/modelo/Usuario.java
@@ -0,0 +1,92 @@
+
+package modelo;
+
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String rut;
+    private String direccion;
+    private String contactoTelefonico;
+    private Perfil perfil;
+    private String username;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getContactoTelefonico() {
+        return contactoTelefonico;
+    }
+
+    public void setContactoTelefonico(String contactoTelefonico) {
+        this.contactoTelefonico = contactoTelefonico;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
