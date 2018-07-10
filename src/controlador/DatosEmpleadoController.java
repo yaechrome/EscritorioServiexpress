c9ddd3db3cda414c1b4aed4b7aba6566f592379a
Joselyn Moya <joselyn.moyap@gmail.com>
2018-07-09 20:12:13 -0400

    Se crea DatosEmpleadoController

---
+++ src/controlador/DatosEmpleadoController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import modelo.*;
+/**
+ *
+ * @author nippo
+ */
+public class DatosEmpleadoController {
+    
+    public boolean crear(DatosEmpleado datos){
+        boolean validar = false;
+        
+        
+        
+        
+        return validar;
+    }
+    
+    public boolean modificar(DatosEmpleado datos){
+        boolean validar = false;
+        
+        
+        
+        
+        return validar;
+    }
+    
+}
